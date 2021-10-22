914b81a2948521328434b85db9864e6ddb410a88
Nathan Andrews <nathoandrews@gmail.com>
2021-10-22 17:20:43 +0100

    Class for storing basic questions

---
+++ BasicQ.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newmcq;
+
+/**
+ *
+ * @author Nathan
+ */
+public class BasicQ extends NewMCQ {
+    
+    public BasicQ(){
+        super();
+    }
+    
+    public BasicQ(String question, String answer, String correctAns, int grade){
+        super(question, answer, correctAns, grade);
+    }
+    
+    public void compute(){
+        
+    }
+}
